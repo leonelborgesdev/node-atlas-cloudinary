2e8af9f3a705fb8a44954394eae820fb414d96ef
leonelborgesdev <leonel03t@gmail.com>
2023-07-22 22:26:12 -0400

    puerto 3000

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("Hola mundo"));
+
+app.listen(3000);
+console.log("Server is running on port", 3000);
