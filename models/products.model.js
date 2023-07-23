cd91ac9202995278c21d306b09c8a179ab9b9163
leonelborgesdev <leonel03t@gmail.com>
2023-07-23 12:04:21 -0400

    product model

---
+++ models/products.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    trim: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+});
+
+export default mongoose.model("Product", productSchema);
