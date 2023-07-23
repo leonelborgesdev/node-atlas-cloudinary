8a422630b920bba4fbed6c29c7937df9091b5d55
leonelborgesdev <leonel03t@gmail.com>
2023-07-22 22:37:11 -0400

    type module

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const morgan = require("morgan");
+import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => res.send("Hola mundo"));
+app.get("/", (req, res) => res.send("Hola mundo2"));
 
 app.listen(3000);
 console.log("Server is running on port", 3000);
