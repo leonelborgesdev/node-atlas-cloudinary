c6575fbce5c6fafe5faac9c66c40359043235266
leonelborgesdev <leonel03t@gmail.com>
2023-07-23 10:47:04 -0400

    index routes

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import indexRoutes from "./routes/index.routes.js";
 
 const app = express();
 
 app.use(morgan("dev"));
-app.get("/", (req, res) => res.send("Hola mundo2"));
 
+app.use(indexRoutes);
 export default app;
