2d914c7c704736040287e64e8d01cec1d7f975b3
leonelborgesdev <leonel03t@gmail.com>
2023-07-23 11:37:17 -0400

    controllers products

---
+++ controllers/products.controllers.js
@@ -0,0 +1,12 @@
+export const getProducts = (req, res) => {
+  res.send("getting products");
+};
+export const createProduct = (req, res) => {
+  res.send("create products");
+};
+export const updateProduct = (req, res) => {
+  res.send("update products");
+};
+export const deleteProduct = (req, res) => {
+  res.send("delete products");
+};
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,6 +1,16 @@
 import { Router } from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/products.controllers";
 const router = Router();
 
-router.get("/products", (req, res) => {
-  res.send("getting products");
-});
+router.get("/products", getProducts);
+
+router.post("/products", createProduct);
+router.put("/products", updateProduct);
+router.delete("/products", deleteProduct);
+
+export default router;
