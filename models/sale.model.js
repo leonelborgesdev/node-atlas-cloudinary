5916905b50292e04c36d6ce22878337bff1df59b
leonelborgesdev <leonel03t@gmail.com>
2023-07-30 08:01:26 -0400

    saleModel

---
+++ models/sale.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const saleSchena = new mongoose.Schema(
+  {
+    total: {
+      type: Number,
+      default: 0,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    idClient: {
+      type: String,
+      require: true,
+      trim: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
